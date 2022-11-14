ea7d3e7efd2479069b9e40e9ad4c09592f6a900f
Amina Alibasic <alibasicamina@outlook.com>
2022-11-14 17:49:23 +0100

    Edited initial schema.sql

--- server/src/main/resources/schema.sql
+++ server/src/main/resources/schema.sql
@@ -1,24 +1,24 @@
+CREATE TABLE enu_user_type (
+      id int NOT NULL PRIMARY KEY,
+      short_code varchar(255),
+      description varchar(255)
+);
+
 CREATE TABLE users (
       id int NOT NULL PRIMARY KEY,
       first_name varchar(255),
       last_name varchar(255),
       date_of_birth date,
+      user_type_id int,
+      FOREIGN KEY(user_type_id) REFERENCES enu_user_type(id),
       isAdmin boolean
-);
-
-CREATE TABLE enu_degree_type (
-      id int NOT NULL PRIMARY KEY,
-      short_code varchar(255),
-      description varchar(255)
 );
 
 CREATE TABLE user_credentials (
       id int NOT NULL PRIMARY KEY,
       username varchar(255),
       password varchar(255),
-      email_address varchar(255),
-      degree_type_id int,
-      FOREIGN KEY(degree_type_id) REFERENCES enu_degree_type(id)
+      email_address varchar(255)
 );
 
 CREATE TABLE enu_employment_type (
@@ -34,9 +34,7 @@ CREATE TABLE departments (
 CREATE TABLE professors (
       id int NOT NULL PRIMARY KEY,
       user_id int,
-      department_id int,
-      FOREIGN KEY(user_id) REFERENCES users(id),
-      FOREIGN KEY(department_id) REFERENCES departments(id)
+      FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
 CREATE TABLE classes (
