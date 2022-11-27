597a8f11704dd53cb6d928a3e823daa6e4c4531a
Amina Alibašić <73773493+amina-alibasic@users.noreply.github.com>
2022-11-27 16:08:47 +0100

    Update IDepartmentService.java

--- server/src/main/java/service/IDepartmentService.java
+++ server/src/main/java/service/IDepartmentService.java
@@ -1,4 +1,9 @@
 package service;
 
+import model.Department;
+
 public interface IDepartmentService {
+    Department addNewDepartment(String departmentName);
+    boolean deleteDepartment(Long departmentId);
+    Department updateDepartmentName(String departmentName);
 }
