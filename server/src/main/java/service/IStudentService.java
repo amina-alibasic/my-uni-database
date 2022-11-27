87c42eadeda45c5ca337d6805cc7b7c4ce9073ab
Amina Alibašić <73773493+amina-alibasic@users.noreply.github.com>
2022-11-27 16:11:13 +0100

    Update IStudentService.java

--- server/src/main/java/service/IStudentService.java
+++ server/src/main/java/service/IStudentService.java
@@ -1,4 +1,9 @@
 package service;
 
+import model.Student;
+
 public interface IStudentService {
+    Student addNewStudent(Student student);
+    boolean deleteStudent(Long studentId);
+    Student updateStudentDepartment(Long studentId, Long newDepartmentId);
 }
