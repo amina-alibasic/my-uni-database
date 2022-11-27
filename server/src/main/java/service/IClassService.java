e6d1dae88479c4a0ec3413b590500c70fc8d48e9
Amina Alibašić <73773493+amina-alibasic@users.noreply.github.com>
2022-11-27 16:08:10 +0100

    Update IClassService.java

--- server/src/main/java/service/IClassService.java
+++ server/src/main/java/service/IClassService.java
@@ -1,4 +1,11 @@
 package service;
 
+import model.Class;
+
 public interface IClassService {
+    Class addNewClass(String className, String departmentName);
+    boolean deleteClass(Long classId);
+    Class updateClass(Class updatedClass);
+    boolean assignClassToNewProfessor(Long classId, Long professorId);
+    boolean moveToAnotherDepartment(Long classId, Long departmentId);
 }
