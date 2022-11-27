535fbb2ce677a46c179fb33a3985dffc3023ecdd
Amina Alibašić <73773493+amina-alibasic@users.noreply.github.com>
2022-11-27 16:10:07 +0100

    Update IEnrollmentService.java

--- server/src/main/java/service/IEnrollmentService.java
+++ server/src/main/java/service/IEnrollmentService.java
@@ -1,4 +1,12 @@
 package service;
 
+import model.Enrollment;
+
 public interface IEnrollmentService {
+    Enrollment addNewEnrollment(Enrollment enrollment);
+    boolean deleteEnrollment(Long enrollmentId);
+    boolean updateEnrollmentClass(Long enrollmentId);
+    boolean updateEnrollmentStudent(Long enrollmentId);
+    boolean editGrade(Integer newGrade, Long enrollmentId);
+    boolean addGrade (Integer newGrade, Long enrollmentId);
 }
