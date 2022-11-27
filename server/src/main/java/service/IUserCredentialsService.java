51612e77c7fb7f163ea018239688c7055880b1b3
Amina Alibašić <73773493+amina-alibasic@users.noreply.github.com>
2022-11-27 16:11:38 +0100

    Update IUserCredentialsService.java

--- server/src/main/java/service/IUserCredentialsService.java
+++ server/src/main/java/service/IUserCredentialsService.java
@@ -1,4 +1,6 @@
 package service;
 
 public interface IUserCredentialsService {
+    boolean updatePassword(String newPassword, Long userId);
+    boolean updateEmailAddress(String newEmailAddress, Long userId);
 }
