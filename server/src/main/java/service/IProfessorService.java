af5e4340f79a77a5a2b3e5cb832ce3709e483141
Amina Alibašić <73773493+amina-alibasic@users.noreply.github.com>
2022-11-27 16:10:36 +0100

    Update IProfessorService.java

--- server/src/main/java/service/IProfessorService.java
+++ server/src/main/java/service/IProfessorService.java
@@ -1,4 +1,9 @@
 package service;
 
+import model.Professor;
+import model.User;
+
 public interface IProfessorService {
+    Professor addNewProfessor(User newUser);
+    boolean deleteProfessor(Long professorId);
 }
