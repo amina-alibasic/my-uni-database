68d70006f49753d4c6cc7a39ec55e96b8f03729e
Amina Alibasic <alibasicamina@outlook.com>
2022-12-07 19:51:25 +0100

    Added background color

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <v-app>
-    <v-main>
-      <LoginHome/>
+    <v-main class="main">
+      <Login/>
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
-import LoginHome from './components/login/LoginHome.vue';
+import Login from './components/login/Login.vue';
 export default {
   name: 'App',
   components: {
-    LoginHome
+    Login
   }
 }
 </script>
+
+
+<style scoped>
+.main {
+    background-color: slateblue;
+    height: 100vh;
+}
+</style>
